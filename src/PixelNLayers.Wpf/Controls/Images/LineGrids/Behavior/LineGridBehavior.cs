472db648f4cea5424f861a2536addfdb88b6aa8d
VectorArtGames <70201838+VectorArtGames@users.noreply.github.com>
2021-11-21 22:14:53 +0100

    Edited UI and more
    + Added Line Grid (Cross - Center point)
    + Removed stackoverflow issue with ResourceDictionary calling itself
    + Added Statusbar styling

---
+++ src/PixelNLayers.Wpf/Controls/Images/LineGrids/Behavior/LineGridBehavior.cs
@@ -0,0 +1,54 @@
+﻿using System.Windows;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using Microsoft.Xaml.Behaviors;
+
+namespace PixelNLayers.Wpf.Controls.Images.LineGrids.Behavior;
+
+internal class LineGridBehavior : Behavior<LineGrid>
+{
+	private const double Unit = 1.333333333;
+
+	/// <inheritdoc />
+	protected override void OnAttached()
+	{
+		AssociatedObject.Loaded += AssociatedObject_Loaded;
+	}
+
+	private void AssociatedObject_Loaded(object sender, RoutedEventArgs e)
+	{
+		Calculate();
+	}
+
+	private void Calculate()
+	{
+		if (AssociatedObject.PixelImageSource is not { } _image) return;
+		int height = _image.Image.PixelHeight;
+		int width = _image.Image.PixelWidth;
+
+		var lines = new Line[2];
+		lines[0] = new Line
+		{
+			X1 = 0,
+			X2 = width * Unit,
+			Y1 = height / 2.0 * Unit,
+			Y2 = height / 2.0 * Unit,
+			Stroke = Brushes.White,
+			StrokeThickness = 0.1,
+			Opacity = 0.8
+		};
+
+		lines[1] = new Line
+		{
+			X1 = width / 2.0 * Unit,
+			X2 = width / 2.0 * Unit,
+			Y1 = -height / 2.0 * Unit,
+			Y2 = height / 2.0 * Unit,
+			Stroke = Brushes.White,
+			StrokeThickness = 0.1,
+			Opacity = 0.8
+		};
+
+		AssociatedObject.Lines = lines;
+	}
+}
---
+++ src/PixelNLayers.Wpf/Controls/Images/LineGrids/LineGrid.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Shapes;
+using PixelNLayers.Shared.Images.Wrapper;
+
+namespace PixelNLayers.Wpf.Controls.Images.LineGrids;
+
+/// <summary>
+///     Interaction logic for LineGrid.xaml
+/// </summary>
+public partial class LineGrid : ItemsControl
+{
+	public static readonly DependencyProperty ShowGridProperty = DependencyProperty.Register(
+		nameof(ShowGrid), typeof(bool), typeof(LineGrid), new PropertyMetadata(default(bool)));
+
+	public static readonly DependencyProperty LinesProperty = DependencyProperty.Register(
+		nameof(Lines), typeof(Line[]), typeof(LineGrid),
+		new PropertyMetadata(Array.Empty<Line>()));
+
+	public static readonly DependencyProperty PixelImageSourceProperty = DependencyProperty.Register(
+		nameof(PixelImageSource), typeof(EditableImage), typeof(LineGrid),
+		new PropertyMetadata(default(EditableImage)));
+
+	public LineGrid()
+	{
+		InitializeComponent();
+	}
+
+	public EditableImage PixelImageSource
+	{
+		get => (EditableImage)GetValue(PixelImageSourceProperty);
+		set => SetValue(PixelImageSourceProperty, value);
+	}
+
+	public Line[] Lines
+	{
+		get => (Line[])GetValue(LinesProperty);
+		set => SetValue(LinesProperty, value);
+	}
+
+	public bool ShowGrid
+	{
+		get => (bool)GetValue(ShowGridProperty);
+		set => SetValue(ShowGridProperty, value);
+	}
+}
